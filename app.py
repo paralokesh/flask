a44f6ecc95c0cfa76d3b78a69f172054d73bbfd3
ParaLokesh Gaddam <paralokesh.gaddam@electrolux.com>
2025-03-25 08:28:48 +0530

    first hosting

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 from flask import Flask
 
+#WSGI Application
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to the Flask application"
+
+if __name__=="__main__":
+    app.run()
